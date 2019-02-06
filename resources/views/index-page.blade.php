6a992a32e109a5968f83c5d6e78fa13f82757e10
vagrant <vagrant@homestead>
2019-02-06 15:23:17 +0000

    20 commit

--- resources/views/index-page.blade.php
+++ resources/views/index-page.blade.php
@@ -20,7 +20,7 @@
 		<nav class="navbar navbar-expand-md navbar-light navbar-laravel sticky-top">
             <div class="container-fluid">
                 <a href="{{ route('page.index') }}">
-                    <img width="220" src="{{asset('img/logo.svg')}}">
+                    <img width="200" src="{{asset('img/logo.svg')}}">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -101,17 +101,77 @@
 						</ul>
 	        		</div>
 	        	</aside>
-	        	<div class="col-8">
-                    <div class="container-fluid p-1" style="background-color: black">
+	        	<div class="col-8" style="background-color: black">
+                    <div class="container-fluid p-1">
                         <div class="row">
                             <div class="col-12 col-md-4 my-auto px-4">
                                 <h1 class="text-white">Доставка суші для істинних поціновувачів</h1>
                                 <p class="text-white">Замовляйте суші преміум класу для всієї компанії додому та в офіс</p>
+                                <b-button style="border-radius: 20px; background: #e16729; border-color: #e16729;">Зробити замовлення</b-button>
                             </div>
                             <div class="col-12 col-md-8">
                                 <img class="img-fluid" src="/img/front/index-page-main.jpg">
                             </div>
                         </div>
+                    </div>
+                    <div class="container-fluid p-1">
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-lg-3 text-center">
+                                <div class="row">
+                                    <div class="col-3 col-lg-12 my-auto">
+                                        <img class="img-fluid" src="/img/front/icons/icon1.svg">
+                                    </div>
+                                    <div class="col-9 col-lg-12 text-white">
+                                        <h4>Fresh Made</h4>
+                                        <p>
+                                            Мы гарантируем свежесть продуктов и не используем замороженную рыбу
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 text-center">
+                                <div class="row">
+                                    <div class="col-3 col-lg-12 my-auto">
+                                        <img class="img-fluid" src="/img/front/icons/icon2.svg">
+                                    </div>
+                                    <div class="col-9 col-lg-12 text-white">
+                                        <h4>Рецепты от шеф-повара</h4>
+                                        <p>
+                                            Мы предлагаем как классические суши, так и авторские рецепты от шеф-повара
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 text-center">
+                                <div class="row">
+                                    <div class="col-3 col-lg-12 my-auto">
+                                        <img class="img-fluid" src="/img/front/icons/icon3.svg">
+                                    </div>
+                                    <div class="col-9 col-lg-12 text-white">
+                                        <h4>Качественные ингредиенты</h4>
+                                        <p>
+                                            Мы не используем усилители вкуса. Все продукты закупаем у проверенных поставщиков
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-3 text-center">
+                                <div class="row">
+                                    <div class="col-3 col-lg-12 my-auto">
+                                        <img class="img-fluid" src="/img/front/icons/icon4.svg">
+                                    </div>
+                                    <div class="col-9 col-lg-12 text-white">
+                                        <h4>Чистая кухня</h4>
+                                        <p>
+                                            Весь процесс приготовления проходит в соответствии со всеми санитарными нормами
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="text-white">Топ позиції</h1>
                     </div>
 	        	</div>
 	        	<div class="col-3">
